fcda54a97775bfbba06b2697a3ee40e10af8aa0e
Lorena Reyes <lorena.n.reyes@gmail.com>
2018-02-20 00:22:19 +0000

    modified function to print avatar URL key only

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,27 @@
 var request = require('request');
+var secrets = require('./secrets.js');
 
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': 'token ' + secrets.GITHUB_TOKEN
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+}
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  var parse = JSON.parse(result);
+  parse.forEach(function(obj) {
+  console.log("Result:" + obj.avatar_url)
+});
+// console.log("Errors:", err);
+ //console.log("Result:", result);
+});
+
